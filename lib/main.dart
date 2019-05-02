4ca1efb693fae4a16f8869353c862252c78f2863
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2019-05-03 03:12:42 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final _title = 'Hello World';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World',
+      title: _title,
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello world'),
+          title: Text(_title),
         ),
         body: Center(
-          child: Text('Hello world'),
+          child: Text(_title),
         ),
       ),
     );
